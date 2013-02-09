597b1c43d6ab310acd0f9630c1d04fed605c90c1
Aljosa Urleb <aljosa.urleb@gmail.com>
2013-02-09 18:07:58 +0100

    Start Fresh

--- lib/Frontend.php
+++ lib/Frontend.php
@@ -7,10 +7,10 @@ class Frontend extends ApiFrontend {
         parent::init();
         // Keep this if you are going to use database on all pages
         //$this->dbConnect();
-        $this->requires('atk','4.2.0');
-        $this->add('jUI');
+        //$this->requires('atk','4.2.0');
+        //$this->add('jUI');
 
- 
+ /*
         $this->add('Menu',null,'Menu')
             ->addMenuItem('index','Welcome')
             ->addMenuItem('examples','Bundled Examples')
@@ -19,6 +19,7 @@ class Frontend extends ApiFrontend {
             ->addMenuItem('authtest','Auth test')
             ->addMenuItem('logout')
             ;
+            */
 
     }
 }
